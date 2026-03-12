85fd0dc28f377c8ece4d5d62de95224bac2eaf06
shravan-kumar-nalacharla <shravankumarnalacharla@gmail.com>
2026-03-12 15:31:57 +0530

    fix: add allowedHosts for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    allowedHosts: ["resolvify-3.onrender.com"]
+  }
 })
